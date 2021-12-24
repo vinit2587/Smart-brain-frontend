954ea833541313880cf3f92433864e199eea2b29
vinit2587 <you@example.com>
2021-12-24 19:58:36 +0530

    Smart brain frontend built from ZTM course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import { Component } from 'react/cjs/react.production.min';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+//import {tsParticles} from "tsparticles";
+
+//const particlesOptions = tsParticles.load("tsparticles", {
+  
+//})
+
+const app = new Clarifai.App({
+  apiKey: 'cbfac01d044e448e92f659a3b24cd12b'
+ });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={this.state.box} ImageUrl={this.state.ImageUrl} />
+      </div>
+    );
+  }
 
 export default App;
